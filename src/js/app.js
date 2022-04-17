4c4d444db07e4ec3db12860c43923de906248303
khanhworktime <chikhanh5@gmail.com>
2022-04-17 12:55:55 +0700

    Finished contact part

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 // Menu responsive
-
+var header = document.querySelector('header');
 var sideMenu = document.querySelector(".side-menu");
 var menuHamBtn = document.querySelector(".menu-ham");
 
@@ -8,3 +8,13 @@ menuHamBtn.addEventListener("click", function () {
     menuHamBtn.classList.toggle("ham-close");
     sideMenu.classList.toggle("closed");
 })
+
+function scrollHandler() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+}
+
+window.onscroll = () => scrollHandler();
